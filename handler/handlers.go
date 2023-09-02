5be475e9469c55885144147ebf3ad60761b96112
Chaitanya <chaitanya.sharma@yocket.com>
2023-09-02 21:31:48 +0530

    add ability to parse hr or hour to start pomo timer until start of next hour

---
+++ handler/handlers.go
@@ -0,0 +1,97 @@
+package handler
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/chaitanyabsprip/pomo/cache"
+)
+
+func stringf(t time.Duration) string {
+	dStr := t.String()
+	var h, m, s string
+	var split []string
+	if strings.Contains(dStr, "h") {
+		split = strings.Split(dStr, "h")
+		h = split[0] + ":"
+		dStr = split[1]
+	}
+	if strings.Contains(dStr, "m") {
+		split = strings.Split(dStr, "m")
+		m = split[0] + ":"
+		dStr = split[1]
+	}
+	if strings.Contains(dStr, "s") {
+		split = strings.Split(dStr, "s")
+		s = split[0]
+	}
+	return fmt.Sprintf("%s%s%s", h, m, s)
+}
+
+func Start(duration time.Duration) {
+	now := time.Now()
+	timer, err := cache.GetTimer()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Something went wrong")
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	var d time.Duration
+	if timer.IsNil() {
+		d = duration
+		defer fmt.Fprintf(os.Stdout, "Started timer for %s\n", duration)
+	} else if timer.IsPaused {
+		d = timer.Duration
+		defer fmt.Fprintf(os.Stdout, "Resumed timer for %s\n", duration)
+	} else {
+		fmt.Fprintln(os.Stderr, "Timer already running", duration)
+		os.Exit(1)
+	}
+	cache.SetTime(cache.Timer{Duration: d, Start: now})
+}
+
+func Pause() {
+	timer, err := cache.GetTimer()
+	if err != nil || timer.IsNil() {
+		fmt.Fprintln(os.Stderr, "No timer running")
+		os.Exit(1)
+	}
+	duration := time.Until(timer.Start.Add(timer.Duration))
+	duration = duration.Round(time.Second)
+	cache.SetTime(cache.Timer{
+		Duration: duration,
+		IsPaused: true,
+		Start:    timer.Start,
+	})
+}
+
+func Stop() {
+	t, _ := cache.GetTimer()
+	if t.IsNil() {
+		fmt.Fprintln(os.Stdout, "No timer running")
+		os.Exit(1)
+	}
+	err := cache.Clear()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't stop timer")
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stdout, "Timer stopped")
+}
+
+func ShowTime() {
+	timer, err := cache.GetTimer()
+	if err != nil || timer.IsNil() {
+		fmt.Fprint(os.Stdout, "")
+		return
+	}
+	if timer.IsPaused {
+		fmt.Fprintf(os.Stdout, "⏸️ %s\n", timer.Duration)
+		return
+	}
+	duration := time.Until(timer.Start.Add(timer.Duration))
+	duration = duration.Round(time.Second)
+	fmt.Fprintf(os.Stdout, "🍅%s\n", stringf(duration))
+}
--- main.go
+++ main.go
@@ -1,120 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
+	"slices"
 	"time"
 
 	"github.com/chaitanyabsprip/pomo/cache"
+	"github.com/chaitanyabsprip/pomo/handler"
 )
 
 var cacheFile = os.Getenv("HOME") + "/.cache/pomotimer"
 
-func stringf(t time.Duration) string {
-	dStr := t.String()
-	var h, m, s string
-	var split []string
-	if strings.Contains(dStr, "h") {
-		split = strings.Split(dStr, "h")
-		h = split[0] + ":"
-		dStr = split[1]
+func parseDuration(args []string) time.Duration {
+	duration, _ := time.ParseDuration("52m")
+	if len(args) > 1 {
+		d := args[1]
+		if slices.Contains([]string{"hour", "hr"}, d) {
+			now := time.Now()
+			nextHour := time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, 0, 0, 0, time.Local)
+			duration = time.Until(nextHour)
+		} else {
+			duration, _ = time.ParseDuration(args[1])
+		}
 	}
-	if strings.Contains(dStr, "m") {
-		split = strings.Split(dStr, "m")
-		m = split[0] + ":"
-		dStr = split[1]
-	}
-	if strings.Contains(dStr, "s") {
-		split = strings.Split(dStr, "s")
-		s = split[0]
-	}
-	return fmt.Sprintf("%s%s%s", h, m, s)
-}
-
-func start(duration time.Duration) {
-	now := time.Now()
-	timer, err := cache.GetTimer()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Something went wrong")
-		fmt.Printf("Error: %s\n", err.Error())
-		os.Exit(1)
-	}
-	var d time.Duration
-	if timer.IsNil() {
-		d = duration
-		defer fmt.Fprintf(os.Stdout, "Started timer for %s\n", duration)
-	} else if timer.IsPaused {
-		d = timer.Duration
-		defer fmt.Fprintf(os.Stdout, "Resumed timer for %s\n", duration)
-	} else {
-		fmt.Fprintln(os.Stderr, "Timer already running", duration)
-		os.Exit(1)
-	}
-	cache.SetTime(cache.Timer{Duration: d, Start: now})
-}
-
-func pause() {
-	timer, err := cache.GetTimer()
-	if err != nil || timer.IsNil() {
-		fmt.Fprintln(os.Stderr, "No timer running")
-		os.Exit(1)
-	}
-	duration := time.Until(timer.Start.Add(timer.Duration))
-	duration = duration.Round(time.Second)
-	cache.SetTime(cache.Timer{
-		Duration: duration,
-		IsPaused: true,
-		Start:    timer.Start,
-	})
-}
-
-func stop() {
-	t, _ := cache.GetTimer()
-	if t.IsNil() {
-		fmt.Fprintln(os.Stdout, "No timer running")
-		os.Exit(1)
-	}
-	err := cache.Clear()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Couldn't stop timer")
-		os.Exit(1)
-	}
-	fmt.Fprintln(os.Stdout, "Timer stopped")
-}
-
-func showTime() {
-	timer, err := cache.GetTimer()
-	if err != nil || timer.IsNil() {
-		fmt.Fprint(os.Stdout, "")
-		return
-	}
-	if timer.IsPaused {
-		fmt.Fprintf(os.Stdout, "⏸️ %s\n", timer.Duration)
-		return
-	}
-	duration := time.Until(timer.Start.Add(timer.Duration))
-	duration = duration.Round(time.Second)
-	fmt.Fprintf(os.Stdout, "🍅%s\n", stringf(duration))
+	return duration
 }
 
 func main() {
-	cmd := ""
-	duration, _ := time.ParseDuration("52m")
-	if len(os.Args) > 1 {
-		cmd = os.Args[1]
+	flag.Parse()
+
+	args := flag.Args()
+	var cmd string
+	if len(args) > 0 {
+		cmd = flag.Args()[0]
 	}
 
 	cache.Setup()
 	switch cmd {
 	case "start":
-		start(duration)
+		duration := parseDuration(args)
+		handler.Start(duration)
 	case "pause":
-		pause()
+		handler.Pause()
 	case "stop":
-		stop()
+		handler.Stop()
 	case "":
-		showTime()
+		handler.ShowTime()
 	default:
 		fmt.Fprintln(os.Stderr, "Invalid command")
 	}
